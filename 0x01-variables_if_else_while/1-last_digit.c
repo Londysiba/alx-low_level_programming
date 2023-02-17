de8c0d87b20c7a8a59276b4715d8d8d95dbf2a55
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-02-17 02:30:14 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+		printf("Last digit of 98 is 8 and is greater than 5\n", n, a);
 	}
 
 	else if (a == 0)
