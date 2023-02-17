8fcab4faf944050e2c2e87d95d1474115332dd7a
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-02-17 02:08:27 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	in n;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -21,7 +21,7 @@ int main(void)
 
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n;
+		printf("%d is zero\n", n);
 	}
 
 	else
