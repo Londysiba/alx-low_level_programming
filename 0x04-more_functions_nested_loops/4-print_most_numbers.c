d66e50defad7377e455487634084a151779a76e2
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-02-23 03:07:48 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,18 +7,21 @@
  */
 
 void print_most_numbers(void)
+
 {
+
 	int x = 0;
+
 	for (; x <= 9; x++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
 	}
 	_putchar('\n');
 }
