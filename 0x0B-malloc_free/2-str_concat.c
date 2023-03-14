7f72065d3613e1e3ec272bcb5104b90acd5328a8
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-03-14 01:04:37 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,61 +1,53 @@
 #include "main.h"
-/**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*Count character of string*/
-	{
-		i++;
-	}
-
-	return (i);
-}
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * str_concat - back a pointer to array
- * @s1: Array one
- * @s2: Array two
- * Return: Always an array dinamic
+ * str_concat - Concatenates two strings of any size
+ * @s1: the first string to concatenate
+ * @s2: the second string to concatenate
+ *
+ * Return: the two strings concatenated
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *dst;
-	unsigned int i, j, size;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
-	/*If the array is empty*/
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	/*count size total*/
-	size = (_strlen(s1) + _strlen(s2) + 1);
+	while (s1[i])
+		i++;
 
-	/*malloc*/
-	dst = (char *) malloc(size * sizeof(char));
+	while (s2[j])
+		j++;
 
-	if (dst == 0)
-	{
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
 		return (NULL);
-	}
 
-	/*Concatenate arrays*/
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + i) = *(s1 + i);
+	j = 0;
 
-	for (j = 0; *(s2 + j) != '\0'; j++)
+	while (k < l)
 	{
-		*(dst + i) = *(s2 + j);
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
 	}
 
-	return (dst);
+	s[k] = '\0';
+	return (s);
 }
