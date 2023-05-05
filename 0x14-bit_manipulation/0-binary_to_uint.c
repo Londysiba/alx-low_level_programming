7361c85307c8e57e6b576aa2c1b87f717b475281
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-05-05 13:54:53 +0200

    git manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int conv_num = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > 'i')
+			return (0);
+		conv_num = 2 * conv_num + (b[i] - '0');
+	}
+
+	return (conv_num);
+}
