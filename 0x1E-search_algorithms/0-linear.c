d66877f9c79d223cdebdce36260f515d0839ee38
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-10-11 15:31:35 +0200

    task 2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
 /**
- * A function that searches for a value in an array of integers using the Linear search algorithm
+ * A function that searches for a value
+ * in an array of integers using the Linear search algorithm
 */
 
 int linear_search(int *array, size_t size, int value)
