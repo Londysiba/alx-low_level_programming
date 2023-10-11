8b3e5943fe6c23dc0abae06950a5e283909e275b
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-10-11 15:20:05 +0200

    task 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+/**
+ * A function that searches for a value in an array of integers using the Linear search algorithm
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, *array);
+		if (*array == value)
+			return (i);
+		array++;
+	}
+	return (-1);
+}
